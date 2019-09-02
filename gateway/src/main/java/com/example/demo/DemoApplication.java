6ca1abbaa8ef8d94cddb866c40e6bf071fa6029c
h_takamatsu <h_takamatsu@kost.jp>
2019-09-02 21:42:15 +0900

    set gateway

--- gateway/src/main/java/com/example/demo/DemoApplication.java
+++ gateway/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,36 @@
 package com.example.demo;
 
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(
+						"custom_express_setpath_route",
+						r -> r.path("/express/{segment}")
+								.filters(f -> f.setPath("/{segment}"))
+								.uri("http://localhost:3000"))
+				.route(
+						"custom_springboot_setpath_route",
+						r -> r.path("/springboot/{segment}")
+								.filters(f -> f.setPath("/{segment}"))
+								.uri("http://localhost:9000"))
+				.build();
 	}
 
 }
